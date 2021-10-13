06b06bcd528c1bfb90e50301d4c22682d39846d1
Adrian Franczak <franczakadrian@gmail.com>
2021-10-13 16:41:27 +0200

    Add DisplayAttribute.cs and Bitmap support

---
+++ NuGet.Revit.Ribbon/Attribute/DisplayAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+
+namespace NuGet.Revit.Ribbon.Attribute
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DisplayAttribute : System.Attribute
+    {
+        //private static readonly Random Random = new Random();
+        public string DisplayName { get; }
+        public int Order { get; }
+        public string PanelName { get; }
+        public Color Color { get; }
+
+        public DisplayAttribute(string displayName, int order, string panelName )
+        {
+            DisplayName = displayName;
+            Order = order;
+            PanelName = panelName;
+            Color = Color.Black;
+            // var localRandom = Random.Next(255);
+            //
+            // var hslColor = new Hsl
+            // {
+            //     H = Random.Next(170,260),
+            //     S = Random.Next(80,100),
+            //     L = Random.Next(40,60)
+            // };
+            // var rgb = hslColor.ToRgb();
+            //
+            // Color = Color.FromArgb((int)rgb.R, (int)rgb.G, (int)rgb.B);
+        }
+        
+        public DisplayAttribute(string displayName, int order, string panelName, Color color )
+        {
+            DisplayName = displayName;
+            Order = order;
+            PanelName = panelName;
+            Color = color;
+        }
+    }
+}
---
+++ NuGet.Revit.Ribbon/PanelsHelper/ImageHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace NuGet.Revit.Ribbon.PanelsHelper
+{
+    public static class ImageHelper
+    {
+        public static Bitmap TransformImage(this string path, int size)
+        {
+            var originalFile =  new Bitmap(path);
+            return new Bitmap(originalFile, size, size);
+        }
+        
+        public static Bitmap TransformImage(this Image image, int size)
+        {
+            return new Bitmap(image, size, size);
+        }
+        
+        public static Bitmap ChangePngColor(this string path, Color color)
+        {
+            var bitmap = new Bitmap(path);
+            return ChangePngColor(bitmap, color);
+        }
+        
+        /// <summary>
+        /// Change black pixels
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Bitmap ChangePngColor(this Bitmap image, Color color)
+        {
+            if (color == Color.Black)
+            {
+                return image;
+            }
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    var pixel = image.GetPixel(x, y).ToArgb();
+                    
+                    if (pixel == Color.Black.ToArgb())
+                    {
+                        image.SetPixel(x, y, color);
+                    }
+                }
+            }
+            return image;
+        }
+        
+        public static BitmapImage ToBitmapImage(this Bitmap src)
+        {
+            var ms = new MemoryStream();
+            src.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            var image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+    }
+}
